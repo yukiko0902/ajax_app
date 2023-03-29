675774a1f6f02325743c2b1ff2c9d2d77c16c816
yukiko0902 <yukikotakeuchi0902@gmail.com>
2023-03-29 18:31:22 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -2,7 +2,7 @@ function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    const form = document.getElementById("form")
+    const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
